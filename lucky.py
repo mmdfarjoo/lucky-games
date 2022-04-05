744f291f70f5d8ef063b8a8263129c57bfb35068
mmd Farjoo <91349670+mmdfarjoo@users.noreply.github.com>
2022-04-05 23:55:07 +0430

    Add files via upload

---
+++ lucky.py
@@ -0,0 +1,206 @@
+nameuser = input('hello user , what your name ?')
+print(f'wellcome {nameuser} to lucky game')
+while(1):
+
+    wellcome = input('menu \n start or help?:')
+
+    if wellcome == 'help':
+        print('-************************-')
+        print('in this game you have to complete ten stages in each level \n in these stages you have to arrange the mixed words :)')
+        continue
+    elif wellcome == 'start':
+        print('lets go...')
+        break
+while(2):
+
+    eachlevel = input(
+        'choose the difficulty level of the game easy , medium , hard\n --->')
+
+    if eachlevel == 'easy':
+        while(11):
+            print('level 1 ---/')
+            charecter = {char for char in 'phone'}
+            print(charecter)
+            level1 = input('---> ')
+            if level1 == 'phone':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(12):
+            print('level 2 ---/')
+            charecter = {char for char in 'woman'}
+            print(charecter)
+            level2 = input('---> ')
+            if level2 == 'woman':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(13):
+            print('level 3 ---/')
+            charecter = {char for char in 'bigbang'}
+            print(charecter)
+            level3 = input('---> ')
+            if level3 == 'bigbang':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+
+        while(13):
+            print('level 4 ---/')
+            charecter = {char for char in 'info'}
+            print(charecter)
+            level4 = input('---> ')
+            if level4 == 'info':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(15):
+            print('level 5 ---/')
+            charecter = {char for char in 'ronaldo'}
+            print(charecter)
+            level5 = input('---> ')
+            if level5 == 'ronaldo':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+
+        continue
+
+
+
+
+
+    elif eachlevel == 'medium':
+        while(111):
+            print('level 1 ---/')
+            charecter = {char for char in 'internet'}
+            print(charecter)
+            level11 = input('---> ')
+            if level11 == 'internet':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(112):
+            print('level 2 ---/')
+            charecter = {char for char in 'googlemap'}
+            print(charecter)
+            level12 = input('---> ')
+            if level12 == 'googlemap':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+
+        while(113):
+            print('level 3 ---/')
+            charecter = {char for char in 'choose'}
+            print(charecter)
+            level13 = input('---> ')
+            if level13 == 'choose':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(114):
+            print('level 4 ---/')
+            charecter = {char for char in 'therock'}
+            print(charecter)
+            level14 = input('---> ')
+            if level14 == 'therock':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(115):
+            print('level 5 ---/')
+            charecter = {char for char in 'bigsmock'}
+            print(charecter)
+            level15 = input('---> ')
+            if level15 == 'bigsmock':
+                print('true')
+                break
+            else:
+                print('false')
+            continue
+
+
+
+        continue
+
+
+    
+    elif eachlevel =='hard':
+        while(1111):
+            print('level 1 ---/')
+            charecter = {char for char in 'translate'}
+            print(charecter)
+            level16 = input('---> ')
+            if level16 == 'translate':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(1112):
+            print('level 2 ---/')
+            charecter = {char for char in 'watching you'}
+            print(charecter)
+            level17 = input('---> ')
+            if level17 == 'watching you':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+
+        while(1113):
+            print('level 3 ---/')
+            charecter = {char for char in 'everydays'}
+            print(charecter)
+            level18 = input('---> ')
+            if level18 == 'everydays':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+
+        while(1114):
+            print('level 4 ---/')
+            charecter = {char for char in 'calculator'}
+            print(charecter)
+            level19 = input('---> ')
+            if level19 == 'calculator':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        while(1115):
+        
+            print('level 5 ---/')
+            charecter = {char for char in 'command line'}
+            print(charecter)
+            level111 = input('---> ')
+            if level111 == 'command line':
+                print('true')
+                break
+            else:
+                print('false')
+                continue
+        continue
